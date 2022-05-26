0f6271a56556fc2ec552f19e2f718ad3fb67c121
16205 <16205@ecam.be>
2022-05-26 11:52:52 +0200

    Add routes

---
+++ apiRouter.js
@@ -0,0 +1,16 @@
+// Imports
+var express = require('express');
+var usersController = require('./controllers/usersController');
+
+// Router
+exports.router = (function() {
+    var apiRouter = express.Router();
+
+    // Users routes
+    apiRouter.route('/users/register/').post(usersController.register);
+    apiRouter.route('/users/login/').post(usersController.login);
+    apiRouter.route('/users/me/').get(usersController.getUserProfile);
+    apiRouter.route('/users/me/').put(usersController.updateUserProfile);
+
+    return apiRouter;
+})();
