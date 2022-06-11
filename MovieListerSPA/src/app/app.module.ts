85c4dbae5f66849e2eb002dc1cbaf99ba6fc8809
16205 <16205@ecam.be>
2022-06-11 16:15:25 +0200

    Some modules and rest service added, not working

--- MovieListerSPA/src/app/app.module.ts
+++ MovieListerSPA/src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { MovieListComponent } from './movie-list/movie-list.component';
+import { MovieComponent } from './movie/movie.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    MovieListComponent,
+    MovieComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
