7ca38c6b7641b2b8d9f0c142eeb70774cfa926d8
16205 <16205@ecam.be>
2022-06-13 17:39:24 +0200

    Add  Logout functionality

--- MovieListerSPA/src/app/app.component.ts
+++ MovieListerSPA/src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { AuthService } from './shared/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'MovieListerSPA';
+
+  constructor(private authService: AuthService) { }
+
+  isLoggedIn: boolean = false;
+
+  ngOnInit() {
+    this.isLoggedIn = this.authService.isLoggedIn;
+  }
+
+  logOut() {
+    this.authService.doLogout();
+    this.isLoggedIn = false;
+  }
 }
